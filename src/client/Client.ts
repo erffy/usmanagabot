48a0e860d671bf1fa6c1130645395458cb0e5bce
erffy <ceyhunerenerguven.07@proton.me>
2024-11-12 15:14:37 +0300

    feat: Implement Loader in Client

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -1,5 +1,6 @@
 import { Client as BaseClient, Collection, Partials } from 'discord.js';
 import Database from './Database';
+import Loader from './Loader';
 
 import database_config from '../../config/database.json';
 
@@ -7,12 +8,15 @@ export default class Client extends BaseClient {
   public readonly commands: Collection<string, BotCommandImplementation> = new Collection();
   public readonly events: Collection<string, BotEventImplementation> = new Collection();
   public readonly database: Database = new Database(database_config);
+  public readonly loader: Loader;
 
   public constructor() {
     super({
       intents: 62991,
       partials: [Partials.Channel, Partials.User, Partials.GuildMember, Partials.Message, Partials.Reaction]
     });
+
+    this.loader = new Loader(this);
 
     (async () => {
       await this.database.initialize();
